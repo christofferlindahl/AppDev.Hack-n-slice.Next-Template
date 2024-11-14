42b55e6b06d5c1370bd30dd39ed0bc56f7217c27
Christoffer Lindahl <christoffer.lindahl@forefront.se>
2024-11-14 11:18:18 +0100

    fix: add MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Container } from "@mui/material";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Container maxWidth="md">{children}</Container>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Typography } from "@mui/material";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return <Typography variant="h1">Hello world</Typography>;
 }
